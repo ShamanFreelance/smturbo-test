96b41b29f6572cecb86bc8476c9e6f7d5c4a5f97
NikFreeman <Classic76@tut.by>
2024-04-17 14:14:05 +0300

    feat: add pagination backend

--- srv/src/users/users.service.ts
+++ srv/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersEntity } from './users.entity';
+import { PageMetaDto, PageOptionsDto } from 'src/common/dtos';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -13,7 +14,13 @@ export class UserService {
   ) {}
 
   // get list of all users
-  async findAll(): Promise<UsersEntity[]> {
-    return await this.usersRepo.find();
+  async findAll(pageOptionsDto: PageOptionsDto): Promise<{ users: UsersEntity[]; pageMetaDto: PageMetaDto }> {
+    const [users, itemCount] = await this.usersRepo.findAndCount({
+      take: pageOptionsDto.take,
+      skip: pageOptionsDto.skip,
+    });
+    const pageMetaDto = new PageMetaDto({ pageOptionsDto, itemCount });
+
+    return { users, pageMetaDto };
   }
 }
